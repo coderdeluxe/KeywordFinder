f7dd001bdb0a935460d3b565b6ca53cb116e7d66
Raymond <coderdeluxe@gmail.com>
2020-08-08 19:18:17 +0800

    first commit

---
+++ KeywordFinder/KeywordFinderHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace KeywordFinder
+{
+    public static class KeywordFinder
+    {
+        /// <summary>
+        /// Return number of occurences base on the inputed word
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static int GetOccurences(string folderPath, string word)
+        {
+            int occurence = 0;
+
+            try
+            {
+                foreach (var line in File.ReadLines(folderPath))
+                {
+                    occurence += Regex.Matches(line, word).Count;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Invalid path: {folderPath}");
+                return -1;
+            }
+
+            return occurence;
+        }
+    }
+}
---
+++ KeywordFinder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Configuration;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace KeywordFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = ConfigurationManager.AppSettings["FolderPath"];
+
+            Console.Write("Please enter word to search: ");
+            var word = Console.ReadLine();
+
+            Console.WriteLine($"Total number of occurence(s) for {word} is {KeywordFinder.GetOccurences(path, word)}");
+  
+            Console.ReadLine();
+        }
+
+    }
+}
